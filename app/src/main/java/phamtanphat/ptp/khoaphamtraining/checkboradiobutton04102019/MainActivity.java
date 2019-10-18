07eb94857435e5e6e2dd46edbed27df965235bf7
phatdroid94 <phatdroid94@gmail.com>
2019-10-18 21:10:36 +0700

    init

---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/checkboradiobutton04102019/MainActivity.java
@@ -0,0 +1,28 @@
+package phamtanphat.ptp.khoaphamtraining.checkboradiobutton04102019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox mCbAndroid,mCbiOS,mCbPhp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCbAndroid = findViewById(R.id.checkboxAndroid);
+        mCbiOS = findViewById(R.id.checkboxios);
+        mCbPhp = findViewById(R.id.checkboxPhp);
+
+        mCbAndroid.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+            }
+        });
+    }
+}
